fa73a8e63a14f0e9cd1af0a1c3f6985099b08bb0
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-16 10:00:46 -0300

    add entidades

---
+++ Business/Entities/Nave.cs
@@ -0,0 +1,12 @@
+namespace Business.Entities
+{
+    public class Nave
+    {
+        public int IdNave { get; set; }
+        public string Nome { get; set; }
+        public string Modelo { get; set; }
+        public int Passageiros { get; set; }
+        public double Carga { get; set; }
+        public string Classe { get; set; }
+    }
+}
