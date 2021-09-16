d2485fdff4c4f8bf768f714a0647d758a4c1e4f6
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-16 11:38:41 -0300

    atualizando onmodelcreating

--- Infra/Data/EstrelaDaMorteDBContext.cs
+++ Infra/Data/EstrelaDaMorteDBContext.cs
@@ -1,3 +1,5 @@
+using Business.Entities;
+using Infra.Data.Mappings;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infra.Data
@@ -8,11 +10,22 @@ namespace Infra.Data
         {
             
         }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new NaveMapping());
+            modelBuilder.ApplyConfiguration(new PilotoMapping());
+            modelBuilder.ApplyConfiguration(new PlanetaMapping());
+            base.OnModelCreating(modelBuilder);
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         { 
-            optionsBuilder.UseSqlServer(@"Server=172.17.0.6;Database=SchoolDB;User Id=sa;Password=App@12345;");
+            optionsBuilder.UseSqlServer(@"Server=172.17.0.6;Database=EstrelaDaMorte;User Id=sa;Password=App@12345;");
         }
-        
+
+        public DbSet<Nave> Naves { get; set; }
+        public DbSet<Piloto> Pilotos { get; set; }
+        public DbSet<Planeta> Planetas { get; set; }
         
     }
 }
