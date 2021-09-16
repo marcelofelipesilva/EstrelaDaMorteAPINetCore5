9a522fa34119bde764bcb9aefbf73c96aeca8e1f
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-16 15:54:10 -0300

    add PilotoViewModelOuput.cs

---
+++ EstrelaDaMorteAPI/Models/Piloto/PilotoViewModelOuput.cs
@@ -0,0 +1,10 @@
+namespace EstrelaDaMorteAPI.Models.Piloto
+{
+    public class PilotoViewModelOuput
+    {
+        public int IdPiloto { get; set; }
+        public string Nome { get; set; }
+        public string AnoNascimento { get; set; }
+        public int IdPlaneta { get; set; }
+    }
+}
