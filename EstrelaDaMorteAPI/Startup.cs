4d2bfda39942ced6540de95c87e9c6dff3a82e52
marcelofelipesilva <marcelofelipesilva@outlook.com>
2021-09-17 18:06:02 -0300

    add configuraçao da connectstring.

--- EstrelaDaMorteAPI/Startup.cs
+++ EstrelaDaMorteAPI/Startup.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Business.Repositories;
+using Infra.Data;
 using Infra.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,11 @@ namespace EstrelaDaMorteAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            Console.WriteLine(Configuration.GetConnectionString("DefaultConnection"));
+            services.AddDbContext<EstrelaDaMorteDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"), x => x.MigrationsAssembly(typeof(EstrelaDaMorteDbContext).Assembly.FullName).EnableRetryOnFailure());
+            });
             services.AddControllers();
             services.AddScoped<INaveRepository, NaveRepository>();
             services.AddScoped<IPilotoRepository, PilotoRepository>();
